9a72cb99cfcfa221f5300de5d3d95d7f304aef84
ShakhzodMirmuminov <s.mirmuminov@student.inha.uz>
2025-11-25 22:43:43 +0500

    created: test.ipynb and data and collector

---
+++ data_collector.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+def fetch_weather_data(latitude, longitude, start_date, end_date, filename):
+    base_url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "hourly": "temperature_2m,relative_humidity_2m,precipitation,windspeed_10m,windgusts_10m,pressure_msl",
+        "timezone": "Asia/Tashkent"
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    
+    if "hourly" in data:
+        df = pd.DataFrame(data["hourly"])
+        df["time"] = pd.to_datetime(df["time"])
+        df.to_csv(filename, index=False)
+        print(f"Data saved to {filename}")
+    else:
+        print("No data received for the given period")
+#TAshkent Coordinates
+latitude = 41.2995
+longitude = 69.2401
+
+start_date = "2010-01-01"
+end_date = "2025-11-23"
+filename = "tashkent_weather_15years.csv"
+
+fetch_weather_data(latitude, longitude, start_date, end_date, filename)
